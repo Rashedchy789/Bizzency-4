64c52e61f9c194cbecb3c812db65172fcb209520
RashedChy <136729495+Rashedchy789@users.noreply.github.com>
2023-09-20 23:06:16 -0700

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,35 @@ $(function () {
     spinner: 'circle',
     border: '10px',
     infinigall: true,
+    infinigall: true,
+    autoplay: true,
     numeration: true,
+    share: true,
+    shareStyle: 'pill',
   });
   AOS.init();
+
+  new TypeIt('#myElement', {
+    speed: 100,
+  })
+    .delete(17)
+    .type('Backend Services')
+    .delete(16)
+    .type('Services')
+    .move(-12)
+    .go();
+
+  new TypeIt('#Element', {
+    speed: 100,
+  }).go();
+
+  new TypeIt('#ment', {
+    speed: 100,
+  }).go();
+
+  new TypeIt('#men', {
+    speed: 100,
+  }).go();
+
+  mixitup('.wrapper');
 });
